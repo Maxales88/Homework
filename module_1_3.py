3407888f359046bb8259ec5283cc08acc41f16cf
Maxales88 <maksimberezner@icloud.com>
2024-12-15 21:45:05 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,12 @@
+name = 'Maxim'
+print('Name: ' + name)
+
+age = int(36)
+print('Age: ' + str(age))
+
+new_age = age + int(1)
+print('New age: ' + str(new_age))
+
+is_student = bool(True)
+print('Is student: ' + str(is_student))
+ 
---
+++ module_1_3_1.py
@@ -0,0 +1,10 @@
+number_of_homework = int(12)
+
+number_of_hours = float(1.5)
+
+course_name = 'Python'
+
+time_for_one_task = number_of_hours / number_of_homework
+
+print('Курс: ',course_name,', всего задач: ',number_of_homework, ', затрачено часов: ', number_of_hours,', среднее время выполнения: ',time_for_one_task,' часа.',sep='')
+

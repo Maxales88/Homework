aa40ad99bffb22b90cbecc893c4dd5769d3dfe17
Maxales88 <maksimberezner@icloud.com>
2025-01-03 22:11:46 +0300

    Update module_3_3.py

--- module_3_3.py
+++ module_3_3.py
@@ -18,5 +18,24 @@ print_params(*values_list)  # Распаковка списка
 print_params(**values_dict)  # Распаковка словаря
 
 # Распаковка и отдельные параметры
-values_list_2 = [76.4, 'Строка']
+values_list_2 = [54.32, 'Строка']
 print_params(*values_list_2, 42)  # Распаковка списка и передача третьего аргумента
+
+def append_to_list(item, list_my=None):
+    if list_my is None:
+        list_my = []  # Создаем новый список, если list_my = None
+    list_my.append(item)  # Добавляем элемент
+    return list_my  # Возвращаем измененный список
+
+
+# Примеры использования функции
+list_1 = append_to_list(1)
+list_2 = append_to_list(2)
+# Передаем новый список
+list_3 = append_to_list(3, values_list_2) 
+list_4 = append_to_list(6, values_list_2)
+list_5 = append_to_list(67, values_list_2)
+
+print("Список 1: ", list_1)
+print("Список 2: ", list_2)
+print("Список 3: ", list_3)

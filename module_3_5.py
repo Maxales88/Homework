a2f28172812e496d9495f05e00b06d35cd8b3ecd
Maxales88 <maksimberezner@icloud.com>
2025-01-06 23:12:04 +0300

    Update module_3_5.py

--- module_3_5.py
+++ module_3_5.py
@@ -6,22 +6,21 @@ def get_multiplied_digits(number):
     if len(str_number) > 1:
         # Берем первую цифру и преобразуем ее в число
         first = int(str_number[0])
-
+        
         # Проверка: если первая цифра не равна 0, перемножаем
         if first != 0:
             return first * get_multiplied_digits(int(str_number[1:]))
         else:
             # Если первая цифра 0, просто вызываем функцию с оставшимися цифрами
             return get_multiplied_digits(int(str_number[1:]))
+            
     else:
-        # Если длина str_number равна 1, проверяем цифру и возвращаем 1 для 0
+        # Если длина str_number равна 1, проверяем цифру и возвращаем 1 для 0 с целью недопущения нулевого произведения
         return int(str_number) if str_number != '0' else 1
 
-
-# Проверим работу функции
 result = get_multiplied_digits(40203)
-print(result)  # Вывод: 24
+print(result)  
 
 result2 = get_multiplied_digits(402030)
-print(result2)  # Вывод: 24
+print(result2)  
 
